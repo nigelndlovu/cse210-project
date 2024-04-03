9fed35ec2afef6cd422bb13a676807d58375eab8
nigelndlovu <ndl20004@byui.edu>
2024-04-03 15:49:45 +0200

    added code to learning05 file

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 5);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Blue", 7, 4);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Green", 8);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
